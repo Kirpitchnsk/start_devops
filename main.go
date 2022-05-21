41f095e9e81d26da85e56da2e1fbdf810164379f
Kirpitchnsk <98724575+Kirpitchnsk@users.noreply.github.com>
2022-05-21 15:10:57 +0700

    Update main.go

--- main.go
+++ main.go
@@ -12,5 +12,5 @@ func main(){
   http.HandleFunc("/hello",func(w http.ResponseWriter, r*http.Request){
     fmt.Fprintf(w, "Hello,user")
   })
-  http.LitsenAndServe(":8098",nil)
+  http.ListenAndServe(":8098",nil)
 }
